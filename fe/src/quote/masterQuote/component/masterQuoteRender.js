51f0bf20f2148b8aa51673e1fd727ed0f4fb31f8
TID0027 <tid0027@w1amacmobile006.ace.aaaclubnet.com>
2018-03-15 15:45:46 -0700

    sub-routes config

--- fe/src/quote/masterQuote/component/masterQuoteRender.js
+++ fe/src/quote/masterQuote/component/masterQuoteRender.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 // import { Field, reduxForm } from 'redux-form';
 import ContactInfoComponent from '../../contactInfo/component/contactInfoComponent'
 import RouteWithSubRoutes from '../../../setup/routes'
 
-let MasterQuoteRender = ( {props, routes} ) => {
+let MasterQuoteRender = ( props ) => {
 
     return (
       <div>
-        <ContactInfoComponent />
+
+        <ul>
+          <li><Link to="/quote/contact">Contact Info</Link></li>
+          <li><Link to="/quote/vehicle">Vehicle Info</Link></li>
+        </ul>
+
+        <Switch>
+          <Route path='/quote/contact' component={ContactInfoComponent}/>
+          <Route path='/quote/vehicle' component={ContactInfoComponent}/>
+        </Switch>
+
       </div>
     )
 
